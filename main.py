c157b2956c9563c2d4b46738011796bd7be09cf7
Ramizimtiaz <rimti2@illinois.edu>
2023-10-24 15:50:45 -0500

    First Sumbission Test

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-# This is a sample Python script.
-from tkinter import*
-
 import GUI
-from Checker import Checker
-
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     gui = GUI.PhishingDetectorGUI()
     gui.run()
